4272f0202a231138d141f36c4f31c37eccc4907b
Cosmotamer <144909695+Cosmotamer@users.noreply.github.com>
2024-02-13 16:48:07 +0800

    Update front.js

--- src/front.js
+++ src/front.js
@@ -2,40 +2,39 @@ const happy_img = document.getElementById('happy_pic');
 const sad_img = document.getElementById('sad_pic');
 const img_cont = document.getElementById('image_container');
 
-// document.getElementById('no_but').addEventListener('mouseenter', function(){
-//     img_cont.classList.add('show');
-//     happy_img.style.display ='none';
-//     sad_img.style.display ='flex';
+document.getElementById('no_but').addEventListener('mouseenter', function(){
+     img_cont.classList.add('show');
+     happy_img.style.display ='none';
+    sad_img.style.display ='flex';
 
-// });
+ });
 
-// document.getElementById('no_but').addEventListener('mouseleave', function(){
-//     img_cont.classList.remove('show');
-//     happy_img.style.display ='none';
-//     sad_img.style.display ='none';
+ document.getElementById('no_but').addEventListener('mouseleave', function(){
+    img_cont.classList.remove('show');
+     happy_img.style.display ='none';
+     sad_img.style.display ='none';
 
-// });
+ });
 
-// document.getElementById('yes_but').addEventListener('mouseenter', function(){
-//     img_cont.classList.add('show');
-//     happy_img.style.display ='flex';
-//     sad_img.style.display ='none';
-// });
+ document.getElementById('yes_but').addEventListener('mouseenter', function(){
+     img_cont.classList.add('show');
+     happy_img.style.display ='flex';
+     sad_img.style.display ='none';
+ });
 
-// document.getElementById('yes_but').addEventListener('mouseleave', function(){
-//     img_cont.classList.remove('show');
-//     happy_img.style.display ='none';
-//     sad_img.style.display ='none';
+ document.getElementById('yes_but').addEventListener('mouseleave', function(){
+     img_cont.classList.remove('show');
+     happy_img.style.display ='none';
+    sad_img.style.display ='none';
 
-// });
+ });
+ document.getElementById('yes_but').addEventListener('click', function(){
 
-// document.getElementById('yes_but').addEventListener('click', function(){
+    document.getElementById('date_place').classList.add('show');  
+    document.getElementById('front_page').style.display ='none';  
+     document.getElementById('date_place').style.display ='flex';
 
-//     document.getElementById('date_place').classList.add('show');  
-//     document.getElementById('front_page').style.display ='none';
-//     document.getElementById('date_place').style.display ='flex';
-
-// });
+ });
 
 
 document.getElementById('choice_4').addEventListener('click' ,function(){
